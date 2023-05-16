97ba5c40903c77ed42ff76bae0f6b92d6de5f972
StudyingAnt <61408144+StudyingAnt@users.noreply.github.com>
2023-05-16 16:55:12 -0400

    Update download_gencode.sh

--- data_downloading_and_preprocessing/download_gencode.sh
+++ data_downloading_and_preprocessing/download_gencode.sh
@@ -6,4 +6,4 @@ release_num=40
 
 # download and unzip data
 wget -P ${DOWNLOAD_PATH} http://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_${release_num}/gencode.v${release_num}.pc_transcripts.fa.gz
-gunzip -c ${DOWNLOAD_PATH}gencode.v${release_num}.pc_transcripts.fa.gz > ${PROJECT_PATH}/exp/${EXP_NAME}/in/gencode.v${release_num}.pc_transcripts.fa
+gunzip -c ${DOWNLOAD_PATH}gencode.v${release_num}.pc_transcripts.fa.gz > ${PROJECT_PATH}gencode.v${release_num}.pc_transcripts.fa
